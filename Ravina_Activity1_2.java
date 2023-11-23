7655baba58d11caab4cc3594255bc8d1410ed9af
MarieRavina <144203969+MarieRavina@users.noreply.github.com>
2023-11-23 22:48:18 +0800

    Add files via upload

---
+++ Ravina_Activity1_2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ravina_activity.pkg1_.pkg2;
+
+/**
+ *
+ * @author My PC
+ */
+public class Ravina_Activity1_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
